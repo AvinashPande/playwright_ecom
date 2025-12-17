d2485f013cc729257783078ddf0c5f45c0d394c8
avinash <avipandey3792@gmail.com>
2025-12-17 18:19:36 +0530

    adding script for iframe handling

---
+++ tests/iFrame.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from "@playwright/test";
+
+test("single iframe", async ({ page }) => {
+  await page.goto("https://demo.automationtesting.in/Frames.html");
+
+  const frameid = page.frameLocator("#singleframe");
+
+  await frameid.locator('[type="text"]').fill("Frame handing");
+
+  await page.waitForTimeout(10000)
+});
+
+test("Nested iframe", async ({ page }) => {
+
+    await page.goto("https://demo.automationtesting.in/Frames.html");
+
+    await page.locator('a[href="#Multiple"]').click()
+      //  await page.bringToFront()
+
+       const outerFrame = page.frameLocator('[src="MultipleFrames.html"]');
+
+       const innerFrame = outerFrame.locator('[src="SingleFrame.html"]').last().contentFrame()
+
+       await innerFrame.locator('[type="text"]').last().fill("inner form handling");
+       await page.waitForTimeout(6000);
+
+
+
+
+});
